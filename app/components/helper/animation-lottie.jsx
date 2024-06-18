7eddef4e7444625aa92202de535d93f49e98b381
Dominika Bemberakova <dominika.bemberakova@gmail.com>
2024-06-18 14:11:11 +0200

    update animation-lottie

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,52 @@
+// "use client";
+
+// import Lottie from "lottie-react";
+
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
+
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
+
+// export default AnimationLottie;
+
 "use client";
 
-import Lottie from "lottie-react";
+import React, { useState, useEffect } from "react";
+import dynamic from 'next/dynamic';
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationPath,
     style: {
-      width: '95%',
+      width: width || '95%',
     }
   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
